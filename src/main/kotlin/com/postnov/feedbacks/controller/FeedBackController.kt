e0ab42080bdb40bf5ba4d34d4871b7eb47560935
apostnov <apostnov@telematika.com>
2024-05-15 15:00:47 +0300

    master

--- src/main/kotlin/com/postnov/feedbacks/controller/FeedBackController.kt
+++ src/main/kotlin/com/postnov/feedbacks/controller/FeedBackController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/feedback")
+@RequestMapping("/feedbacks")
 @CrossOrigin
 class FeedBackController(
     @Qualifier("gptLlamaApiService") val gptAnswerService: GptAnswerService
